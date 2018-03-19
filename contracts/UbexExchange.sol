bfc8e1f68d707859a54376dcf48882a3809564cb
Dan Gartman <dan.gartman@hotmail.com>
2018-03-19 19:31:29 +0100

    ubex exchange smart contract prototype

---
+++ contracts/UbexExchange.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.23;
+
+// common zeppelin includes
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+/**
+ * @title UbexExchange
+ * @dev UBEX exchange main system contract
+ */
+contract UbexExchange {
+    using SafeMath for uint256;
+
+}
