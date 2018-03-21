454adc600e0f83ecfded4525ff21ae478b8d19a8
Dan Gartman <dan.gartman@hotmail.com>
2018-03-21 22:36:14 +0100

    ownable exchange

--- contracts/UbexExchange.sol
+++ contracts/UbexExchange.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.4.23;
 
 // common zeppelin includes
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import './zeppelin/math/SafeMath.sol';
+
+// ubex-related includes
+import './SystemOwner.sol';
 
 /**
  * @title UbexExchange
@@ -9,5 +12,32 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
  */
 contract UbexExchange {
     using SafeMath for uint256;
+
+    // stores a list of system addresses who have access to crucial functionality
+    SystemOwner public systemOwner;
+
+    // check that sender actually has access to crucial functionality
+    modifier onlyOwner() {
+        require(systemOwner.isOwner(msg.sender));
+        _;
+    }
+
+    /**
+    * @param systemOwnerAddress ethereum address of the access control smart-contract
+    */
+    function UbexExchange(
+        address systemOwnerAddress
+    ) public {
+        systemOwner = SystemOwner(systemOwnerAddress);
+    }
+
+    /**
+    * Set new ethereum address of the access control smart-contract
+    *
+    * @param systemOwnerAddress ethereum address of the access control smart-contract
+    */
+    function setSystemOwner(address systemOwnerAddress) public onlyOwner {
+        systemOwner = SystemOwner(systemOwnerAddress);
+    }
 
 }
