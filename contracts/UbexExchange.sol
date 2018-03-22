d2cc9b0500aa61539a8706d2cf83a78c543e232e
Dan Gartman <dan.gartman@hotmail.com>
2018-03-22 21:06:16 +0100

    users

--- contracts/UbexExchange.sol
+++ contracts/UbexExchange.sol
@@ -16,11 +16,31 @@ contract UbexExchange {
     // stores a list of system addresses who have access to crucial functionality
     SystemOwner public systemOwner;
 
+    // the role of the user
+    enum Role {Undefined, Publisher, Advertiser}
+
+    // user's data structure
+    struct User {
+        // unix timestamp
+        uint created;
+        // ethereum address of the user
+        address owner;
+        // the role of the user
+        Role role;
+        // name of the user
+        string name;
+        // details of the user (ideally URL to the full details)
+        string details;
+    }
+
     // check that sender actually has access to crucial functionality
     modifier onlyOwner() {
         require(systemOwner.isOwner(msg.sender));
         _;
     }
+
+    // list of users
+    mapping (bytes16 => User) public users;
 
     /**
     * @param systemOwnerAddress ethereum address of the access control smart-contract
