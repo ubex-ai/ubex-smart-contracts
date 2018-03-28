38f7a81719c8b52371716086080020f68c807e2d
Dan Gartman <dan.gartman@hotmail.com>
2018-03-28 21:28:16 +0100

    use storage in exchange contract

--- contracts/UbexExchange.sol
+++ contracts/UbexExchange.sol
@@ -5,6 +5,7 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 // ubex-related includes
 import './SystemOwner.sol';
+import './UbexStorage.sol';
 
 /**
  * @title UbexExchange
@@ -15,23 +16,10 @@ contract UbexExchange {
 
     // stores a list of system addresses who have access to crucial functionality
     SystemOwner public systemOwner;
+    // separate contract for system data storage
+    UbexStorage public store;
 
-    // the role of the user
-    enum Role {Undefined, Publisher, Advertiser}
-
-    // user's data structure
-    struct User {
-        // unix timestamp
-        uint created;
-        // ethereum address of the user
-        address owner;
-        // the role of the user
-        Role role;
-        // name of the user
-        string name;
-        // details of the user (ideally URL to the full details)
-        string details;
-    }
+    event PublisherCreated(bytes16 indexed id, address indexed owner);
 
     // check that sender actually has access to crucial functionality
     modifier onlyOwner() {
@@ -39,15 +27,15 @@ contract UbexExchange {
         _;
     }
 
-    // list of users
-    mapping (bytes16 => User) public users;
-
     /**
+    * @param storageAddress ethereum address of the storage smart-contract
     * @param systemOwnerAddress ethereum address of the access control smart-contract
     */
     function UbexExchange(
-        address systemOwnerAddress
+    address storageAddress,
+    address systemOwnerAddress
     ) public {
+        store = UbexStorage(storageAddress);
         systemOwner = SystemOwner(systemOwnerAddress);
     }
 
@@ -58,6 +46,35 @@ contract UbexExchange {
     */
     function setSystemOwner(address systemOwnerAddress) public onlyOwner {
         systemOwner = SystemOwner(systemOwnerAddress);
+    }
+
+    /**
+    * Set new ethereum address of the storage smart-contract
+    *
+    * @param storageAddress ethereum address of the storage smart-contract
+    */
+    function setStorageAddress(address storageAddress) public onlyOwner {
+        store = UbexStorage(storageAddress);
+    }
+
+    /**
+    * Create new publisher account and store its data to the system storage smart-contract
+    *
+    * @param id UUID of the publisher, should be unique withing a system
+    * @param owner ethereum address of the publisher (actually receiver of the UBEX tokens)
+    * @param name name of the publisher for visual identification by the system participants
+    * @param details details of the publisher, ideally an URL with the full data available
+    * @param rank initial system rank of the publisher
+    * @param state initial status of the publisher (should be New in most cases)
+    */
+    function createPublisher(bytes16 id, address owner, string name, string details, UbexStorage.Rank rank, UbexStorage.State state) public onlyOwner {
+        UbexStorage.State _state;
+        (, _state) = store.users(id);
+        require(_state == UbexStorage.State.Unknown);
+
+        store.setUser(id, owner, UbexStorage.Role.Publisher, name, details, rank, state);
+
+        emit PublisherCreated(id, owner);
     }
 
 }
