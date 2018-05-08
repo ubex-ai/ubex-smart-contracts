224c465604d6fe43a61869b1523098ab9834acb5
Dan Gartman <dan.gartman@hotmail.com>
2018-05-08 16:14:46 +0100

    change sol version

--- contracts/UbexToken.sol
+++ contracts/UbexToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import 'zeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
