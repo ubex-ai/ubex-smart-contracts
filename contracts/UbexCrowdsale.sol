015734312d456d22de61c2bc7da08e1df5066d73
Dan Gartman <dan.gartman@hotmail.com>
2018-05-14 23:05:52 +0100

    add event for token delivery, add rate multiplier

--- contracts/UbexCrowdsale.sol
+++ contracts/UbexCrowdsale.sol
@@ -11,8 +11,45 @@ import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 contract UbexCrowdsale is Crowdsale, Ownable {
     using SafeMath for uint256;
 
+    // Map of all purchaiser's balances (doesn't include bounty amounts)
     mapping(address => uint256) public balances;
+
+    // Amount of issued tokens
+    uint256 public tokensIssued;
+
+    // bonus tokens rate multiplier x1000 (i.e. 1200 is +20% = 1.2 = 1200 / 1000)
+    uint256 public bonusMultiplier;
+
+    // Is a crowdsale closed?
     bool public closed;
+
+    /**
+     * Event for token withdrawal logging
+     * @param receiver who receive the tokens
+     * @param amount amount of tokens sent
+     */
+    event TokenDelivered(address indexed receiver, uint256 amount);
+
+    /**
+    * Init crowdsale by setting its params
+    *
+    * @param _rate Number of token units a buyer gets per wei
+    * @param _wallet Address where collected funds will be forwarded to
+    * @param _token Address of the token being sold
+    * @param _bonusMultiplier bonus tokens rate multiplier x1000
+    */
+    function UbexCrowdsale(
+        uint256 _rate,
+        address _wallet,
+        ERC20 _token,
+        uint256 _bonusMultiplier
+    ) Crowdsale(
+        _rate,
+        _wallet,
+        _token
+    ) {
+        bonusMultiplier = _bonusMultiplier;
+    }
 
     /**
      * @dev Withdraw tokens only after crowdsale ends.
@@ -28,6 +65,16 @@ contract UbexCrowdsale is Crowdsale, Ownable {
      */
     function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
         balances[_beneficiary] = balances[_beneficiary].add(_tokenAmount);
+        tokensIssued = tokensIssued.add(_tokenAmount);
+    }
+
+    /**
+   * @dev Overrides the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount.mul(rate).mul(bonusMultiplier).div(1000);
     }
 
     /**
@@ -53,7 +100,14 @@ contract UbexCrowdsale is Crowdsale, Ownable {
     }
 
     /**
-     * @dev Withdraw tokens axcess on the contract after crowdsale.
+     * @dev set the bonus multiplier.
+     */
+    function setBonusMultiplier(uint256 bonusMultiplier_) public onlyOwner {
+        bonusMultiplier = bonusMultiplier_;
+    }
+
+    /**
+     * @dev Withdraw tokens excess on the contract after crowdsale.
      */
     function postCrowdsaleWithdraw(uint256 _tokenAmount) public onlyOwner {
         token.transfer(wallet, _tokenAmount);
@@ -67,6 +121,7 @@ contract UbexCrowdsale is Crowdsale, Ownable {
         uint256 amount = balances[receiver_];
         require(amount > 0);
         balances[receiver_] = 0;
+        emit TokenDelivered(receiver_, amount);
         _deliverTokens(receiver_, amount);
     }
 }
